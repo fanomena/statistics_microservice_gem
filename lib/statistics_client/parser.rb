d461d796e83ca539c1dca1603fca7a138b3bab7c
Rasmus <firestarter2100@gmail.com>
2019-02-27 14:38:56 +0100

    Cleaned up parser and added utm data parsing

--- lib/statistics_client/parser.rb
+++ lib/statistics_client/parser.rb
@@ -12,23 +12,22 @@ module StatisticsClient
 
       # IP/Region/City data - We delete IP due to GDPR
       if data[:ip]
-        geodata       = lookup_ip(data[:ip])
-        data[:ip]     = mask_ip(data[:ip])
-        data[:region] = geodata.region
-        data[:city]   = geodata.city
+        data.merge(lookup_ip(data[:ip]))
+        data[:ip] = mask_ip(data[:ip])
       end
 
       # Parse user agent into usable device information
       if request.user_agent
-        user_agent_data    = parse_user_agent(request.user_agent)
-        data[:browser]     = user_agent_data[:browser]
-        data[:os]          = user_agent_data[:os]
-        data[:device_type] = user_agent_data[:device_type]
-        data[:device_name] = user_agent_data[:device_name]
+        data.merge(parse_user_agent(request.user_agent))
       end
 
       # Set referer header if present
       data[:referer] = request.referer if request.referer
+
+      # Gather utm data
+      if request.original_url
+        data.merge(utm_properties(request.original_url, request.params))
+      end
 
       # Convert all non-standard fields into tracking_data value
       data[:tracking_data] ||= {}
@@ -44,7 +43,11 @@ module StatisticsClient
     private
 
       def self.lookup_ip(ip)
-        Geocoder.search(ip).first
+        geo_data = Geocoder.search(ip).first
+        {
+          region: geo_data.region,
+          city: geo_data.city
+        }
       end
 
       def self.normalize_event_name(name)
@@ -74,6 +77,18 @@ module StatisticsClient
           device_type: device_type,
           device_name: agent.device_name
         }
+      end
+
+      # Gather utm properties from URL
+      def self.utm_properties(url, params)
+        landing_uri = Addressable::URI.parse(url) rescue nil
+        landing_params = (landing_uri && landing_uri.query_values) || {}
+
+        props = {}
+        %w(utm_source utm_medium utm_term utm_content utm_campaign).each do |name|
+          props[name.to_sym] = params[name] || landing_params[name]
+        end
+        props
       end
 
       # Mask an IP according to the Google specification:
